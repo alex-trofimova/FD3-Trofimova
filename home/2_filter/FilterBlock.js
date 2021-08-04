2e5b1bc6283ca777146c23d11cd478d02306ced8
alex-trofimova <alex.trofimova@gmail.com>
2021-08-04 22:44:23 +0300

    hometask2_2

--- home/2_filter/FilterBlock.js
+++ home/2_filter/FilterBlock.js
@@ -6,6 +6,7 @@ var FilterBlock = React.createClass({
       initWordsList: React.PropTypes.array.isRequired, //массив из слов
       startShowMode: React.PropTypes.number.isRequired, // состояние отображения списка: 1 - без сортировки, 2 - сортировка в алфавитном порядке
       initfilterFieldText: React.PropTypes.string.isRequired, // содержимое текстового поля - параметр фильтровки
+      initisSorted: React.PropTypes.bool.isRequired, // состояние чекбокса: true - checked = true, false - checked = false
     },
 
 
@@ -14,28 +15,32 @@ var FilterBlock = React.createClass({
         filterFieldText:this.props.initfilterFieldText,
         showMode:this.props.startShowMode,
         wordsList:this.props.initWordsList,
+        isSorted:this.props.initisSorted,
       };
     },
 
     // описание функции при вводе букв в текстовом поле - меняется соответствующее значение state
-    filterFieldTextChanged: function(request) { 
-      this.setState( {filterFieldText:request} ); 
+    filterFieldTextChanged: function(EO) { 
+      this.setState( {filterFieldText:EO.target.value} ); 
     },
 
     //описание функции при нажатии кнопки сброс - возврат к исходному состоянию
     returnToInitState: function(){
       this.setState( {showMode:1} );
-      this.setState( {filterFieldText:this.props.initfilterFieldText} );
+      this.setState( {isSorted:false} );
+      this.setState( {filterFieldText:''} );
     },
 
     //описание функции при работе с чекбоксом - для сортировки
     sortWords: function() {
       if (this.state.showMode==1) {
         this.setState( {showMode:2} );
+        this.setState( {isSorted:true} );
       }
 
       if (this.state.showMode==2) {
         this.setState( {showMode:1} );
+        this.setState( {isSorted:false} );
       } 
     },
 
@@ -66,13 +71,12 @@ var FilterBlock = React.createClass({
 
           React.DOM.div( {className:'FilterOptions'},
             //чекбокс
-            React.DOM.input( {type:'checkbox',defaultChecked:false, onChange:this.sortWords} ),
-
-            //отрисовка дочернего компонента
-            React.createElement(FilterOption, 
-              {filterFieldText:this.state.filterFieldText,
-              cbfilterFieldTextChanged:this.filterFieldTextChanged,  
-              } ),
+            React.DOM.input( {type:'checkbox',checked:this.state.isSorted, 
+                              onClick:this.sortWords} ),
+            //текстовое поле
+            React.DOM.input( {type:'text',name:'CutOption',className:'FilterField',
+                              value:this.state.filterFieldText, 
+                              onChange:this.filterFieldTextChanged} ),
             //кнопка сброс
             React.DOM.input( {type:'button',value:'сброс', onClick:this.returnToInitState} ),
           ),
