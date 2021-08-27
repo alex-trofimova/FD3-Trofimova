f697fa6c387b73b72de9810de52c122f2e5373bd
alex-trofimova <alex.trofimova@gmail.com>
2021-08-27 10:07:36 +0300

    hometask5_1

---
+++ home/5_rainbowFrame/components/Rainbow.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Rainbow.css';
+
+import RainbowFrame from './RainbowFrame';
+
+class Rainbow extends React.Component {
+
+  render() {
+  let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+    
+  return (
+        <RainbowFrame colors={colors}>
+          Hello!
+        </RainbowFrame>
+    )
+    ;
+
+  }
+
+}
+
+export default Rainbow;
---
+++ home/5_rainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    colors: PropTypes.array.isRequired,
+  };
+  
+  render() {
+    let setOfFrames = this.props.colors.map((elem,index) => 
+    <div key={index} style={{border:"solid 4px "+elem, position: 'absolute', 
+                  left: 5*index+'px', top: 5*index+'px',
+                  width: (400-10*index)+'px', 
+                  height: (200-10*index)+'px'}}/>
+    );
+    
+    return (
+      <div className="Rainbow">
+        {setOfFrames} 
+        <p className='RainbowContent'>{this.props.children}</p>
+      </div>
+    );
+  }
+
+}
+
+export default RainbowFrame;
